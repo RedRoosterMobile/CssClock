bb28318732032f82668a20834a000568cfe0bd38
Thomas Ranker <thomas.ranker@sofatutor.com>
2019-07-27 12:31:40 +0200

    use animationframe

--- js/core-home.js
+++ js/core-home.js
@@ -1,22 +1,25 @@
+var hourElement, minuteElement, secondElement;
+
 function updateClock() {
   var date = new Date();
   var second = date.getSeconds() * 6,
     minute = date.getMinutes() * 6 + second / 60,
     hour = ((date.getHours() % 12) / 12) * 360 + 90 + minute / 12;
 
-  document.getElementById("hour").style =
-    "transform: " + "rotate(" + hour + "deg)";
-  document.getElementById("minute").style =
-    "transform: " + "rotate(" + minute + "deg)";
-  document.getElementById("second").style =
-    "transform: " + "rotate(" + second + "deg)";
+  hourElement.style = "transform: " + "rotate(" + hour + "deg)";
+  minuteElement.style = "transform: " + "rotate(" + minute + "deg)";
+  secondElement.style = "transform: " + "rotate(" + second + "deg)";
 }
 
-function timedUpdate() {
+function timedUpdate(timestamp) {
   updateClock();
-  setTimeout(timedUpdate, 1000);
+  window.requestAnimationFrame(timedUpdate);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+  hourElement = document.getElementById("hour");
+  minuteElement = document.getElementById("minute");
+  secondElement = document.getElementById("second");
   timedUpdate();
+  requestAnimationFrame(timedUpdate);
 });
